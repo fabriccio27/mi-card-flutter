ead24a5318498d6c6f3bda1529cc838c28534922
fabriccio27 <fabricio_inc27@hotmail.com>
2024-01-08 12:09:41 -0300

    Add first information stripe

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,35 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontFamily: 'SourceCodePro',
                   fontSize: 20.0,
-                  color: Colors.teal.shade900,
+                  color: Colors.teal.shade50,
                   letterSpacing: 2.5,
-                  fontWeight: FontWeight.w600,
+                  fontWeight: FontWeight.w400,
                 ),
-                )
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+54 2974 009209',
+                      style: TextStyle(
+                        fontFamily: 'SourceCodePro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.w100,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
           ),
