692e32bdd71c577d2d69e338645b2d8bdda60e12
fabriccio27 <fabricio_inc27@hotmail.com>
2024-01-08 10:37:51 -0300

    Play with layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,23 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100.0,
+                height: double.infinity,
                 width: 100.0,
                 color: Colors.red,
-                child: Text('Container 3'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
               ),
             ] 
           ),
